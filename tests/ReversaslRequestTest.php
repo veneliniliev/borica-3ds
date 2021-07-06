601630715bf20fd82f0a75f6d024ba30c69a05a4
Blum <blum@i-creativ.net>
2021-07-06 09:54:36 +0300

    Cleaning test alredy executed.

--- tests/ReversaslRequestTest.php
+++ tests/ReversaslRequestTest.php
@@ -6,7 +6,6 @@
 
 namespace VenelinIliev\Borica3ds\Tests;
 
-use VenelinIliev\Borica3ds\SaleRequest;
 use VenelinIliev\Borica3ds\ReversalRequest;
 use VenelinIliev\Borica3ds\Exceptions\SignatureException;
 use VenelinIliev\Borica3ds\Exceptions\ParameterValidationException;
@@ -51,17 +50,5 @@ class ReversaslRequestTest extends TestCase
             'MERCHANT' => self::MERCHANT_ID,
             'P_SIGN' => '765C034CC1AC9213B59AE0D251C48B3883AA44DF73A57E4E12F73786B4847F42D40AF5CAFC33F2A065DF5EF28C095FBA373F4A9D08CD27CCEA9447CFB49A80404A1CB3067223C8AF0964C6A7E7960C43CB4B3C75FF9063C8931C0457CB6B43D9F536DD32950AF90E05A887079149415D1FFA1017ED716696D256FE9DBF69D6C4CB10AB71F60C7405A90E4E111CE37C62901A02188A74D7BA84FABB02E0D877DA998E29DEF0057CD5EAF5CAD0C6132EDB2A9CAE0556FD360F26BD47869802B0EB3C40D1205524AEA0FF5CF413A887F66DD032FCD09C5281C834B072BAEDF92950F4BCE2DA32A4A6E9392B46F9FA55BD08E64F16EE096CCD43DA4F077FFC5A9700'
         ], $saleData);
-    }
-
-    /**
-     * @return void
-     * @throws ParameterValidationException|SignatureException
-     */
-    public function testBackRefValidation()
-    {
-        $this->expectException(ParameterValidationException::class);
-
-        (new SaleRequest())
-            ->setBackRefUrl('wrong url value');
     }
 }
