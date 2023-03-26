32ff8eb9b5dea8b5bc61b3878f8fa47a054e3ca0
Venelin Iliev <venelin@provision.bg>
2023-03-26 12:33:02 +0300

    TestCase remove abstract (php 8+ error)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,7 @@
 
 namespace VenelinIliev\Borica3ds\Tests;
 
-abstract class TestCase extends \PHPUnit\Framework\TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
     const TERMINAL_ID = 'V1800001';
     const MERCHANT_ID = '1600000001';
